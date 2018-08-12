055168864a95a88f08335c990bc849a8af0b716e
xuchengpeng <330476629@qq.com>
2018-08-12 08:42:51 +0800

    startup with nw.

--- test-startup.sh
+++ test-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 
-emacs -l init.test.el --eval "(add-hook 'dotemacs-post-init-hook #'dotemacs|run-test t)"
+emacs -nw -l init.test.el --eval "(add-hook 'dotemacs-post-init-hook #'dotemacs|run-test t)"

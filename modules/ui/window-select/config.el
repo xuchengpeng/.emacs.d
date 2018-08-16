71a438d007b25b660ff4e71dc0d2739471fd01c5
xuchengpeng <330476629@qq.com>
2018-08-16 14:05:14 +0800

    Customize window-select module with feature +ace-window and +switch-window.

--- modules/ui/window-select/config.el
+++ modules/ui/window-select/config.el
@@ -1,8 +1,8 @@
 ;;; ui/window-select/config.el -*- lexical-binding: t; -*-
 
-(use-package ace-window
-  :bind ("M-o" . ace-window))
-
-(use-package switch-window
-  :disabled
-  :bind ("M-o" . switch-window))
+(cond ((featurep! +switch-window)
+       (use-package switch-window
+         :bind ("M-o" . switch-window)))
+      ((or (featurep! +ace-window) t)
+       (use-package ace-window
+         :bind ("M-o" . ace-window))))
--- modules/ui/window-select/packages.el
+++ modules/ui/window-select/packages.el
@@ -1,6 +1,10 @@
 ;;; ui/window-select/packages.el -*- lexical-binding: t; -*-
 
-(package! ace-window
-          ;; switch-window
-          )
+(cond ((featurep! +switch-window)
+       ;; Install switch-window if the user indicated the '+switch-window' module flag
+       (package! switch-window))
+      ((or (featurep! +ace-window) t)
+       ;; Install ace-window if the user selects the flag '+ace-window' or by default
+       ;; ... if the user did not specify a module flag
+       (package! ace-window)))
 

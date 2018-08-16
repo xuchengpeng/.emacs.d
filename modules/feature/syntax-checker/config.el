687a7da12cd461eda6d24b7c555ce8bcd132714e
xuchengpeng <330476629@qq.com>
2018-08-16 20:05:14 +0800

    Customize flycheck module.

--- modules/feature/syntax-checker/config.el
+++ modules/feature/syntax-checker/config.el
@@ -1,4 +1,6 @@
 ;;; feature/syntax-checker/config.el -*- lexical-binding: t; -*-
 
 (use-package flycheck
-  :hook (after-init . global-flycheck-mode))
+  :hook (after-init . global-flycheck-mode)
+  :config
+  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)))

f7ce812c88c0cb0e0df9c3ff8f645ca02ef9aaa8
xuchengpeng <330476629@qq.com>
2018-08-21 10:56:58 +0800

    Use yas-minor-mode.

--- modules/feature/snippets/config.el
+++ modules/feature/snippets/config.el
@@ -2,8 +2,12 @@
 
 (use-package yasnippet
   :diminish yas-minor-mode
-  :hook (after-init . yas-global-mode)
+  ;; :hook (after-init . yas-global-mode)
+  :hook ((prog-mode     . yas-minor-mode)
+         (snippet-mode  . yas-minor-mode)
+         (markdown-mode . yas-minor-mode)
+         (org-mode      . yas-minor-mode))
   :config
   (setq yas-snippet-dirs (list (concat dotemacs-dir "snippets/")))
   ;; (use-package yasnippet-snippets :ensure t)
-  )
+  (yas-reload-all))

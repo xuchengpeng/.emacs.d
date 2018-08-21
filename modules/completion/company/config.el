e828b35fa5f0d97c642b24ecb08682860208797e
xuchengpeng <330476629@qq.com>
2018-08-21 11:12:11 +0800

    Customize company-global-modes.

--- modules/completion/company/config.el
+++ modules/completion/company/config.el
@@ -3,23 +3,24 @@
 (use-package company
   :diminish company-mode
   :bind (("M-/" . company-complete)
-         ("C-c C-y" . company-yasnippet)
-         :map company-active-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next)
-         ("TAB" . company-complete-common-or-cycle)
-         ("<tab>" . company-complete-common-or-cycle)
-         :map company-search-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next))
+          ("C-c C-y" . company-yasnippet)
+          :map company-active-map
+          ("C-p" . company-select-previous)
+          ("C-n" . company-select-next)
+          ("TAB" . company-complete-common-or-cycle)
+          ("<tab>" . company-complete-common-or-cycle)
+          :map company-search-map
+          ("C-p" . company-select-previous)
+          ("C-n" . company-select-next))
   :hook (after-init . global-company-mode)
-  :config
+  :init
   (setq company-idle-delay 0.2
         company-tooltip-limit 20
         company-tooltip-align-annotations t
         company-minimum-prefix-length 2
         company-dabbrev-downcase nil
         company-dabbrev-ignore-case nil
-        company-require-match nil)
-  
+        company-require-match nil
+        company-global-modes '(not comint-mode erc-mode message-mode help-mode gud-mode eshell-mode))
+  :config
   (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))

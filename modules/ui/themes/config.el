325819dd752b509a14622d53059ed7e6ed3d4e64
xuchengpeng <330476629@qq.com>
2018-08-23 10:38:00 +0800

    Customize themes module.

--- modules/ui/themes/config.el
+++ modules/ui/themes/config.el
@@ -2,26 +2,29 @@
 
 (defun dotemacs-set-custom-theme (theme)
   "Set dotemacs-themes"
-  (require 'dotemacs-themes)
-  
-  ;; Global settings (defaults)
-  (setq dotemacs-themes-enable-bold t    ; if nil, bold is universally disabled
-        dotemacs-themes-enable-italic t) ; if nil, italics is universally disabled
-  
-  ;; Load the theme (dotemacs-one, dotemacs-molokai, etc); keep in mind that each theme
-  ;; may have their own settings.
-  (load-theme theme t)
-  
-  ;; Enable flashing mode-line on errors
-  (dotemacs-themes-visual-bell-config)
-  
-  ;; Corrects (and improves) org-mode's native fontification.
-  (dotemacs-themes-org-config)
-  
-  ;; For treemacs users
-  (dotemacs-themes-treemacs-config)
-  ;; or for neotree users
-  ;;(dotemacs-themes-neotree-config)
+  (use-package dotemacs-themes
+    :load-path "modules/ui/themes/dotemacs-themes"
+    :config
+    ;; Global settings (defaults)
+    (setq dotemacs-themes-enable-bold t    ; if nil, bold is universally disabled
+          dotemacs-themes-enable-italic t) ; if nil, italics is universally disabled
+    
+    ;; Load the theme (dotemacs-one, dotemacs-molokai, etc); keep in mind that each theme
+    ;; may have their own settings.
+    (load-theme theme t)
+    
+    ;; Enable flashing mode-line on errors
+    (dotemacs-themes-visual-bell-config)
+    
+    ;; Corrects (and improves) org-mode's native fontification.
+    (dotemacs-themes-org-config)
+    
+    ;; For treemacs users
+    (when (featurep! :ui treemacs)
+      (dotemacs-themes-treemacs-config))
+    ;; or for neotree users
+    (when (featurep! :ui neotree)
+      (dotemacs-themes-neotree-config)))
   
   (message "Set custom color theme '%s'." theme))
 
@@ -42,9 +45,6 @@
    
    (t
     (error "Unknown color theme: '%s'" theme))))
-
-(let ((dir (concat (DIR!) "dotemacs-themes/")))
-  (add-to-list 'load-path dir))
 
 ;; Color theme
 (dotemacs-set-theme dotemacs-color-theme)

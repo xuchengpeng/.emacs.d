3ebabbb87ca44ce4a5c53758846dda71bebbf1ed
xuchengpeng <330476629@qq.com>
2018-08-25 09:08:44 +0800

    Customize treemacs.

--- modules/ui/treemacs/config.el
+++ modules/ui/treemacs/config.el
@@ -4,32 +4,20 @@
   :commands (treemacs)
   :bind ("<f8>" . treemacs)
   :config
-  (setq treemacs-deferred-git-apply-delay   0.5
-        treemacs-display-in-side-window     t
-        treemacs-file-event-delay           5000
-        treemacs-follow-after-init          t
-        treemacs-follow-recenter-distance   0.1
-        treemacs-goto-tag-strategy          'refetch-index
-        treemacs-indentation                2
-        treemacs-indentation-string         " "
-        treemacs-is-never-other-window      nil
-        treemacs-no-png-images              nil
-        treemacs-project-follow-cleanup     nil
-        treemacs-persist-file               (expand-file-name "treemacs-persist" dotemacs-cache-dir)
-        treemacs-recenter-after-file-follow nil
-        treemacs-recenter-after-tag-follow  nil
-        treemacs-show-hidden-files          t
-        treemacs-silent-filewatch           nil
+  (setq treemacs-follow-after-init          t
+        treemacs-width                      35
+        treemacs-position                   'left
+        treemacs-is-never-other-window      t
         treemacs-silent-refresh             nil
+        treemacs-indentation                2
         treemacs-sorting                    'alphabetic-desc
-        treemacs-space-between-root-nodes   t
-        treemacs-tag-follow-cleanup         t
-        treemacs-tag-follow-delay           1.5
-        treemacs-width                      35)
+        treemacs-show-hidden-files          t
+        treemacs-goto-tag-strategy          'refetch-index
+        treemacs-display-in-side-window     t
+        treemacs-persist-file               (expand-file-name "treemacs-persist" dotemacs-cache-dir))
   
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
-  (treemacs-fringe-indicator-mode t)
   
   (pcase (cons (not (null (executable-find "git")))
                (not (null (executable-find "python3"))))

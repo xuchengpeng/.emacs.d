7086004462f919ba915e915846458f957eb4073e
xuchengpeng <330476629@qq.com>
2018-08-31 09:55:22 +0800

    Customize dashboard module.

--- modules/ui/dashboard/config.el
+++ modules/ui/dashboard/config.el
@@ -1,13 +1,6 @@
 ;;; ui/dashboard/config.el -*- lexical-binding: t; -*-
 
-(defvar +dashboard-startup-banner 'official
-   "Specify the startup banner. Default value is `official', it displays
-the official spacemacs logo. An integer value is the index of text
-banner, `random' chooses a random text banner in `core/banners'
-directory. A string value must be a path to a .PNG file.
-If the value is nil then no banner is displayed.")
-
-(defvar +dashboard-banner-directory (concat (DIR!) "banners/"))
+(load! "+banner")
 
 (defconst +dashboard-buffer-window-width 80
   "Current width of the home buffer if responsive, 80 otherwise.
@@ -18,6 +11,9 @@ See `+dashboard-startup-buffer-responsive'.")
 
 (defun +dashboard/init-dashboard ()
   "Initialize dashboard."
+  (setq +dashboard-buffer-window-width (if +dashboard-startup-buffer-responsive
+                                           (window-width)
+                                         80))
   (+dashboard/insert-banner))
 
 (add-hook 'dotemacs-post-init-hook #'+dashboard/init-dashboard t)

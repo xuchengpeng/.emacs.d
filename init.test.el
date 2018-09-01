ea7e203b7a6064ad782addffcf134c3129399e0e
xuchengpeng <330476629@qq.com>
2018-09-01 11:14:24 +0800

    Customize emacs startup

--- init.test.el
+++ init.test.el
@@ -1,9 +1,5 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-(unless (bound-and-true-p early-init-file)
-  (load (concat (file-name-directory load-file-name) "early-init")
-        nil t))
-
 (require 'core (concat user-emacs-directory "core/core"))
 
 (dotemacs! :feature

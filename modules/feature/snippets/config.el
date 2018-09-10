e68eff0ab8c63f6643dcc1bc417a455f2e9cc044
xuchengpeng <330476629@qq.com>
2018-09-10 13:47:58 +0800

    Customize snippets module

--- modules/feature/snippets/config.el
+++ modules/feature/snippets/config.el
@@ -1,14 +1,12 @@
 ;;; feature/snippets/config.el -*- lexical-binding: t; -*-
 
-(defvar +snippets-dir (concat (DIR!) "snippets/"))
+(defvar +snippets-dir (concat (DIR!) "snippets/")
+  "Directory where `yasnippet' will search for your private snippets.")
 
 (use-package yasnippet
   :diminish yas-minor-mode
   ;; :hook (after-init . yas-global-mode)
-  :hook ((prog-mode     . yas-minor-mode)
-         (snippet-mode  . yas-minor-mode)
-         (markdown-mode . yas-minor-mode)
-         (org-mode      . yas-minor-mode))
+  :hook ((prog-mode snippet-mode markdown-mode org-mode) . yas-minor-mode)
   :config
   (setq yas-snippet-dirs (list +snippets-dir))
   ;; (use-package yasnippet-snippets :ensure t)

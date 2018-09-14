a1a71e8fc98dab6a3f5b07266d39667e66da1d49
xuchengpeng <330476629@qq.com>
2018-09-14 09:13:03 +0800

    add dotemacs/set-frame-opacity command

---
+++ core/autoload/ui.el
@@ -0,0 +1,13 @@
+;;; core/autoload/ui.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun dotemacs/set-frame-opacity (opacity)
+  "Interactively change the current frame's opacity.
+
+OPACITY is an integer between 0 to 100, inclusive."
+  (interactive
+   (list (read-number "Opacity (0-100): "
+                      (or (frame-parameter nil 'alpha)
+                          100))))
+  (set-frame-parameter nil 'alpha opacity))
+

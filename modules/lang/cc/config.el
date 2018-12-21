c358dc138d4a7c0ccb47460afc4514c3eea7fbeb
xuchengpeng <330476629@qq.com>
2018-12-21 17:12:55 +0800

    add clang-format for C language

--- modules/lang/cc/config.el
+++ modules/lang/cc/config.el
@@ -7,4 +7,5 @@
             (setq c-basic-offset 4
                   default-tab-width 4
                   tab-width 4
-                  indent-tabs-mode nil)))
+                  indent-tabs-mode nil)
+            (load (concat dotemacs-vendor-dir "clang-format.el") t (not dotemacs-debug-mode))))

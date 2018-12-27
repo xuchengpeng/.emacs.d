b562c030ae75b88d521816e4ac1321af9f323486
xuchengpeng <330476629@qq.com>
2018-12-27 12:33:27 +0800

    Refactor which-key

--- modules/feature/keybinds/config.el
+++ modules/feature/keybinds/config.el
@@ -4,7 +4,18 @@
   :defer 1
   :diminish which-key-mode
   :commands which-key-mode
-  :config (which-key-mode +1))
+  :init
+  (setq which-key-sort-order #'which-key-prefix-then-key-order
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10)
+  :config
+  (set-face-attribute 'which-key-local-map-description-face nil :weight 'bold)
+  (which-key-setup-side-window-bottom)
+  (setq-hook! 'which-key-init-buffer-hook line-spacing 3)
+  (which-key-mode +1))
 
 (add-hook 'after-init-hook
           (lambda ()

86b4af88f117ebca10b3d6b4e346f7907d9494a3
xuchengpeng <330476629@qq.com>
2019-02-26 09:13:20 +0800

    Remove magithub

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -1,17 +1,9 @@
 ;;; tools/magit/config.el -*- lexical-binding: t; -*-
 
 (use-package magit
-  :defer t
+  :commands magit-file-delete
   :config
   (setq magit-auto-revert-mode nil))
-
-(use-package magithub
-  :after magit
-  :preface
-  (setq magithub-dir (concat dotemacs-cache-dir "magithub/"))
-  :config
-  (magithub-feature-autoinject t)
-  (setq magithub-clone-default-directory "~/"))
 
 (use-package magit-todos
   :hook (magit-mode . magit-todos-mode))
--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,5 +1,4 @@
 ;;; tools/magit/packages.el -*- lexical-binding: t; -*-
 
 (package! magit)
-(package! magithub)
 (package! magit-todos)

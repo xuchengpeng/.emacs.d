429a7d6f8a9cc8ea7bae204475434571e46d1795
xuchengpeng <330476629@qq.com>
2019-03-06 08:32:29 +0800

    Refactor treemacs

--- modules/ui/treemacs/config.el
+++ modules/ui/treemacs/config.el
@@ -3,27 +3,13 @@
 (use-package treemacs
   :commands (treemacs)
   :config
-  (setq treemacs-follow-after-init          t
-        treemacs-width                      35
-        treemacs-position                   'left
-        treemacs-is-never-other-window      t
-        treemacs-silent-refresh             nil
-        treemacs-indentation                2
-        treemacs-sorting                    'alphabetic-desc
-        treemacs-show-hidden-files          t
-        treemacs-goto-tag-strategy          'refetch-index
-        treemacs-display-in-side-window     t
-        treemacs-persist-file               (expand-file-name "treemacs-persist" dotemacs-cache-dir))
+  (setq treemacs-follow-after-init     t
+        treemacs-is-never-other-window t
+        treemacs-sorting               'alphabetic-case-insensitive-desc
+        treemacs-persist-file          (expand-file-name "treemacs-persist" dotemacs-cache-dir))
   
-  (treemacs-follow-mode t)
-  (treemacs-filewatch-mode t)
-  
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null (executable-find "python3"))))
-    (`(t . t)
-     (treemacs-git-mode 'extended))
-    (`(t . _)
-     (treemacs-git-mode 'simple))))
+  ;; Don't follow the cursor
+  (treemacs-follow-mode -1))
 
 (use-package treemacs-projectile
   :after treemacs projectile)

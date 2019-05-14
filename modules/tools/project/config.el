920f26c99a9efc336d6c5a98944231859e5adf38
xuchengpeng <330476629@qq.com>
2019-05-14 10:37:52 +0800

    projectile: fall back to git-grep instead of rgrep
    Also changes projectile-sort-order, but this only affects the native indexer.

--- modules/tools/project/config.el
+++ modules/tools/project/config.el
@@ -8,7 +8,10 @@
         projectile-known-projects-file (concat dotemacs-cache-dir "projectile.projects")
         projectile-globally-ignored-files '(".DS_Store" "Icon" "TAGS")
         projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o")
-        projectile-ignored-projects '("~/" "/tmp"))
+        projectile-ignored-projects '("~/" "/tmp")
+        projectile-files-cache-expire 604800 ; expire after a week
+        projectile-sort-order 'recentf
+        projectile-use-git-grep t)
   (setq-default projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
 
   (projectile-mode +1))

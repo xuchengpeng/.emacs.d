138f31d91beb1eb13e674acb3af5503f323d7a51
xuchengpeng <330476629@qq.com>
2019-07-06 08:32:51 +0800

    Fix: tabs messing up formatting in treemacs-icons-dired-mode

--- vendor/dotemacs-themes/dotemacs-themes-ext-treemacs.el
+++ vendor/dotemacs-themes/dotemacs-themes-ext-treemacs.el
@@ -28,10 +28,13 @@ variable-pitch face."
   (when (display-graphic-p)
     (set-window-fringes nil 0 0)))
 
-(defun dotemacs-themes-setup-treemacs (&rest _)
-  "Set up `line-spacing' and `tab-width'."
-  (setq line-spacing dotemacs-themes-treemacs-line-spacing
-        tab-width 1))
+(defun dotemacs-themes-setup-tab-width (&rest _)
+  "Set `tab-width' to 1, so tab characters don't ruin formatting."
+  (setq tab-width 1))
+
+(defun dotemacs-themes-setup-line-spacing ()
+  "Set `line-spacing' in treemacs buffers."
+  (setq line-spacing dotemacs-themes-treemacs-line-spacing))
 
 (defun dotemacs-themes-hide-modeline ()
   (setq mode-line-format nil))
@@ -55,12 +58,20 @@ variable-pitch face."
          face nil :inherit
          `(variable-pitch ,@(delq 'unspecified (if (listp faces) faces (list faces)))))))))
 
+(defun dotemacs-themes-fix-treemacs-icons-dired-mode ()
+  "Set `tab-width' to 1 in dired-mode if `treemacs-icons-dired-mode' is active."
+  (if treemacs-icons-dired-mode
+      (add-hook 'dired-mode-hook #'dotemacs-themes-setup-tab-width nil t)
+    (remove-hook 'dired-mode-hook #'dotemacs-themes-setup-tab-width t)))
 
 ;;
 ;;; Bootstrap
 
 (with-eval-after-load 'treemacs
-  (add-hook 'treemacs-mode-hook #'dotemacs-themes-setup-treemacs)
+  (add-hook 'treemacs-mode-hook #'dotemacs-themes-setup-tab-width)
+  (add-hook 'treemacs-mode-hook #'dotemacs-themes-setup-line-spacing)
+  
+  (add-hook 'treemacs-icons-dired-mode-hook #'dotemacs-themes-fix-treemacs-icons-dired-mode)
 
   ;; The modeline isn't useful in treemacs
   (add-hook 'treemacs-mode-hook #'dotemacs-themes-hide-modeline)

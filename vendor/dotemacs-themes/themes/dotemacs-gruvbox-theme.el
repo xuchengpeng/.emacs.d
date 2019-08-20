ab66cfaf679349aa1681de5aa89d8cc6c9a1571e
xuchengpeng <330476629@qq.com>
2019-08-20 08:14:50 +0800

    Clean:Mod: Added compiler pacifier. Removed org level 1 height

--- vendor/dotemacs-themes/themes/dotemacs-gruvbox-theme.el
+++ vendor/dotemacs-themes/themes/dotemacs-gruvbox-theme.el
@@ -234,7 +234,7 @@ determine the exact padding."
    (markdown-code-face :background (dotemacs-lighten base2 0.045))
 
    ;; org-mode
-   (org-level-1 :foreground yellow :bold bold :height 1.2)
+   (org-level-1 :foreground yellow :bold bold)
    (org-level-2 :inherit 'org-level-1 :foreground cyan)
    (org-level-3 :bold bold :foreground green)
    (org-level-4 :inherit 'org-level-3)

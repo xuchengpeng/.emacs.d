84ec3a318141a5bb5b42af1fea2c51601a2afbc1
xuchengpeng <330476629@qq.com>
2019-10-11 17:39:43 +0800

    Refactor modeline module

--- modules/ui/modeline/config.el
+++ modules/ui/modeline/config.el
@@ -1,5 +1,5 @@
 ;;; ui/modeline/config.el -*- lexical-binding: t; -*-
 
-(add-to-list 'load-path (concat dotemacs-vendor-dir "dotemacs-modeline"))
+(add-to-list 'load-path (concat (dir!) "/dotemacs-modeline"))
 (require 'dotemacs-modeline)
 (add-hook 'after-init-hook #'dotemacs-modeline-mode)

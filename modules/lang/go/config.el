bfae3dff269090ce352f1193b44571ffd08a642c
xuchengpeng <330476629@qq.com>
2019-10-15 14:58:42 +0800

    Add go commands

--- modules/lang/go/config.el
+++ modules/lang/go/config.el
@@ -1,7 +1,20 @@
 ;;; lang/go/config.el -*- lexical-binding: t; -*-
 
 (use-package go-mode
-  :mode ("\\.go\\'" . go-mode))
+  :mode ("\\.go\\'" . go-mode)
+  :config
+  (map-local! go-mode-map
+    "b"  '(:ignore t       :which-key "build")
+    "br" '(+go/run         :which-key "go run .")
+    "bb" '(+go/build       :which-key "go build")
+    "bc" '(+go/clean       :which-key "go clean")
+    "f"  '(gofmt           :which-key "gofmt")
+    "i"  '(go-goto-imports :which-key "go-goto-imports")
+    "t"  '(:ignore t       :which-key "test")
+    "tt" '(+go/test-rerun  :which-key "test rerun")
+    "ta" '(+go/test-all    :which-key "test all")
+    "ts" '(+go/test-single :which-key "test single")
+    "tn" '(+go/test-nested :which-key "test nested")))
 
 (when (featurep! :completion company)
   (use-package company-go

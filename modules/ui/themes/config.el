6df8b3292aaedf1abcc62c76ee4bc08bcb8414fa
xuchengpeng <330476629@qq.com>
2019-10-18 17:49:41 +0800

    ui/themes: straight dotemacs-themes from github

--- modules/ui/themes/config.el
+++ modules/ui/themes/config.el
@@ -3,33 +3,31 @@
 (defvar +themes-color-theme nil
   "The color theme to load.")
 
-(defvar +themes-dir (concat (dir!) "/dotemacs-themes")
-  "Directoriy for your private themes.")
-
 (defun +themes-init-theme-h ()
   "Set the theme."
-  (add-to-list 'load-path +themes-dir)
-  
-  (require 'dotemacs-themes)
-  
-  ;; Enable flashing mode-line on errors
-  (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-visual-bell)
-                                               (dotemacs-themes-visual-bell-config)))
-  
-  ;; Corrects (and improves) org-mode's native fontification.
-  (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-org)))
-  
-  ;; For treemacs users
-  (when (featurep! :ui treemacs)
-    (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-treemacs))))
-  ;; or for neotree users
-  (when (featurep! :ui neotree)
-    (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-neotree))))
-  
-  (unless +themes-color-theme
-    (setq +themes-color-theme 'dotemacs-one))
-  
-  (when (and +themes-color-theme (not (memq +themes-color-theme custom-enabled-themes)))
-    (load-theme +themes-color-theme t)))
+  (use-package dotemacs-themes
+    :straight (dotemacs-themes :type git :host github
+                               :repo "xuchengpeng/dotemacs-themes"
+                               :files ("*.el" "themes/*.el"))
+    :config
+    ;; Enable flashing mode-line on errors
+    (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-visual-bell)
+                                                 (dotemacs-themes-visual-bell-config)))
+
+    ;; Corrects (and improves) org-mode's native fontification.
+    (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-org)))
+
+    ;; For treemacs users
+    (when (featurep! :ui treemacs)
+      (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-treemacs))))
+    ;; or for neotree users
+    (when (featurep! :ui neotree)
+      (add-hook 'dotemacs-load-theme-hook (lambda! (require 'dotemacs-themes-ext-neotree))))
+
+    (unless +themes-color-theme
+      (setq +themes-color-theme 'dotemacs-one))
+
+    (when (and +themes-color-theme (not (memq +themes-color-theme custom-enabled-themes)))
+      (load-theme +themes-color-theme t))))
 
 (add-hook 'dotemacs-init-ui-hook #'+themes-init-theme-h)

fd874c8103c7a97fa65f30238fdbf07ae864af2a
xuchengpeng <330476629@qq.com>
2019-11-17 15:22:28 +0800

    Refactor sudo files

--- core/autoload/files.el
+++ core/autoload/files.el
@@ -1,18 +1,25 @@
 ;;; core/autoload/files.el -*- lexical-binding: t; -*-
 
+(defun dotemacs--sudo-file (file)
+  (let ((host (or (file-remote-p file 'host) "localhost")))
+    (concat "/" (when (file-remote-p file)
+                  (concat (file-remote-p file 'method) ":"
+                          (if-let (user (file-remote-p file 'user))
+                              (concat user "@" host)
+                            host)
+                          "|"))
+            "sudo:root@" host
+            ":" (or (file-remote-p file 'localname)
+                    file))))
+
 ;;;###autoload
 (defun dotemacs/sudo-find-file (file)
   "Open FILE as root."
-  (interactive
-   (list (read-file-name "Open as root: ")))
-  (when (file-writable-p file)
-    (user-error "File is user writeable, aborting sudo"))
-  (find-file (if (file-remote-p file)
-                 (concat "/" (file-remote-p file 'method) ":" (file-remote-p file 'user) "@" (file-remote-p file 'host)  "|sudo:root@" (file-remote-p file 'host) ":" (file-remote-p file 'localname))
-               (concat "/sudo:root@localhost:" file))))
+  (interactive "FOpen file as root: ")
+  (find-file (dotemacs--sudo-file file)))
 
 ;;;###autoload
 (defun dotemacs/sudo-this-file ()
   "Open the current file as root."
   (interactive)
-  (dotemacs/sudo-find-file (file-truename buffer-file-name)))
+  (find-alternate-file (dotemacs--sudo-file buffer-file-name)))

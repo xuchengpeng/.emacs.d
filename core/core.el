50b0909278e8ea3885c691d2b587d632475cd534
xuchengpeng <330476629@qq.com>
2019-12-06 08:40:11 +0800

    add plist support

--- core/core.el
+++ core/core.el
@@ -10,7 +10,7 @@
 ;;
 ;;; Global variables
 
-(defconst EMACS27+ (> emacs-major-version 26))
+(defconst EMACS27+   (> emacs-major-version 26))
 (defconst IS-MAC     (eq system-type 'darwin))
 (defconst IS-LINUX   (eq system-type 'gnu/linux))
 (defconst IS-WINDOWS (memq system-type '(cygwin windows-nt ms-dos)))

8972fbd82b1e09120a11df32e89fe217414d9c84
xuchengpeng <330476629@qq.com>
2021-04-07 18:14:05 +0800

    Refactor helm keybindings

--- modules/completion/helm/config.el
+++ modules/completion/helm/config.el
@@ -10,7 +10,7 @@
     [remap bookmark-jump]               'helm-bookmarks
     [remap execute-extended-command]    'helm-M-x
     [remap find-file]                   'helm-find-files
-    [remap imenu-anywhere]              'helm-imenu-anywhere
+    [remap locate]                      'helm-locate
     [remap imenu]                       'helm-semantic-or-imenu
     [remap noop-show-kill-ring]         'helm-show-kill-ring
     [remap switch-to-buffer]            'helm-buffers-list
@@ -23,7 +23,7 @@
   
   (define-key! helm-map
     "TAB"           'helm-execute-persistent-action
-    "<tab>"         'helm-execute-persistent-action
+    "[tab]"         'helm-execute-persistent-action
     "C-z"           'helm-select-action)
   :config
   (setq helm-candidate-number-limit 50
@@ -32,8 +32,6 @@
         helm-mode-line-string nil
         helm-ff-auto-update-initial-value nil
         helm-find-files-doc-header nil
-        ;; Don't override evil-ex's completion
-        helm-mode-handle-completion-in-region nil
         ;; Default helm window sizes
         helm-display-buffer-default-width nil
         helm-display-buffer-default-height 0.25
@@ -44,21 +42,15 @@
         helm-ff-lynx-style-map nil)
   
   (let ((fuzzy (featurep! +fuzzy)))
-    (setq helm-M-x-fuzzy-match fuzzy
-          helm-ag-fuzzy-match fuzzy
-          helm-apropos-fuzzy-match fuzzy
-          helm-apropos-fuzzy-match fuzzy
+    (setq helm-apropos-fuzzy-match fuzzy
           helm-bookmark-show-location fuzzy
           helm-buffers-fuzzy-matching fuzzy
-          helm-completion-in-region-fuzzy-match fuzzy
-          helm-completion-in-region-fuzzy-match fuzzy
           helm-ff-fuzzy-matching fuzzy
           helm-file-cache-fuzzy-match fuzzy
           helm-flx-for-helm-locate fuzzy
           helm-imenu-fuzzy-match fuzzy
           helm-lisp-fuzzy-completion fuzzy
           helm-locate-fuzzy-match fuzzy
-          helm-mode-fuzzy-match fuzzy
           helm-projectile-fuzzy-match fuzzy
           helm-recentf-fuzzy-match fuzzy
           helm-semantic-fuzzy-match fuzzy))
@@ -68,10 +60,10 @@
 
   (helm-mode +1))
 
-(when (featurep! +fuzzy)
-  (use-package helm-flx
-    :hook (helm-mode . helm-flx-mode)
-    :config (helm-flx-mode +1)))
+(use-package helm-flx
+  :when (featurep! +fuzzy)
+  :hook (helm-mode . helm-flx-mode)
+  :config (helm-flx-mode +1))
 
 (use-package helm-projectile
   :commands (helm-projectile-find-file
@@ -87,11 +79,11 @@
   :after xref
   :config (setq xref-show-xrefs-function #'helm-xref-show-xrefs))
 
-(when (featurep! :lang org)
-  (use-package helm-org
-    :defer t
-    :init
-    (after! helm-mode
-      (pushnew! helm-completing-read-handlers-alist
-                '(org-capture . helm-org-completing-read-tags)
-                '(org-set-tags . helm-org-completing-read-tags)))))
+(use-package helm-org
+  :when (featurep! :lang org)
+  :defer t
+  :init
+  (after! helm-mode
+    (pushnew! helm-completing-read-handlers-alist
+              '(org-capture . helm-org-completing-read-tags)
+              '(org-set-tags . helm-org-completing-read-tags))))

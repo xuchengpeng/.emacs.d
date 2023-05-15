f53f660b5df2f65c15cfae0d8e90804e32a3018b
xuchengpeng <330476629@qq.com>
2023-05-15 20:11:25 +0800

    fix(flycheck): bind flycheck-command-map with C-c f

--- modules/dotemacs-flycheck.el
+++ modules/dotemacs-flycheck.el
@@ -15,7 +15,10 @@
   :config
   (delq 'new-line flycheck-check-syntax-automatically)
   (setq flycheck-idle-change-delay 1.0
-        flycheck-display-errors-delay 0.25))
+        flycheck-display-errors-delay 0.25)
+  (define-key flycheck-mode-map flycheck-keymap-prefix nil)
+  (setq flycheck-keymap-prefix (kbd "C-c f"))
+  (define-key flycheck-mode-map flycheck-keymap-prefix flycheck-command-map))
 
 (provide 'dotemacs-flycheck)
 ;;; dotemacs-flycheck.el ends here

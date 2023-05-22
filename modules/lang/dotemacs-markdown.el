4ccf2ed6118409d85135fb20821ee3bc674891d3
xuchengpeng <330476629@qq.com>
2023-05-22 09:20:54 +0800

    fix(markdown): bind local leader with markdown-mode-style-map

--- modules/lang/dotemacs-markdown.el
+++ modules/lang/dotemacs-markdown.el
@@ -7,26 +7,7 @@
 (use-package markdown-mode
   :mode ("/README\\(?:\\.md\\)?\\'" . gfm-mode)
   :init
-  (add-to-list 'dotemacs-localleader-mode-map-alist '(markdown-mode . markdown-mode-map))
-  :config
-  (define-key markdown-mode-map "o" #'markdown-open)
-  (define-key markdown-mode-map "p" #'markdown-preview)
-  (define-key markdown-mode-map "e" #'markdown-export)
-  (define-key markdown-mode-map "i" '("Insert" . (keymap)))
-  (define-key markdown-mode-map "iT" #'markdown-toc-generate-toc)
-  (define-key markdown-mode-map "ib" #'markdown-insert-bold)
-  (define-key markdown-mode-map "ic" #'markdown-insert-code)
-  (define-key markdown-mode-map "ie" #'markdown-insert-italic)
-  (define-key markdown-mode-map "if" #'markdown-insert-footnote)
-  (define-key markdown-mode-map "ih" #'markdown-insert-header-dwim)
-  (define-key markdown-mode-map "ii" #'markdown-insert-image)
-  (define-key markdown-mode-map "ik" #'markdown-insert-kbd)
-  (define-key markdown-mode-map "il" #'markdown-insert-link)
-  (define-key markdown-mode-map "ip" #'markdown-insert-pre)
-  (define-key markdown-mode-map "iq" #'markdown-insert-blockquote)
-  (define-key markdown-mode-map "is" #'markdown-insert-strike-through)
-  (define-key markdown-mode-map "it" #'markdown-insert-table)
-  (define-key markdown-mode-map "iw" #'markdown-insert-wiki-link))
+  (add-to-list 'dotemacs-localleader-mode-map-alist '(markdown-mode . markdown-mode-style-map)))
 
 (provide 'dotemacs-markdown)
 ;;; dotemacs-markdown.el ends here

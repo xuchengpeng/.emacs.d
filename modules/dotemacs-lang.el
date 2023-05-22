44c93ff592dc8572a21bca566fe15b8ba73db015
xuchengpeng <330476629@qq.com>
2023-05-22 09:45:59 +0800

    fix(lang): add lua-mode

--- modules/dotemacs-lang.el
+++ modules/dotemacs-lang.el
@@ -5,6 +5,7 @@
 (add-to-list 'load-path (expand-file-name "lang/" dotemacs-modules-dir))
 
 (require 'dotemacs-markdown)
+(require 'dotemacs-lua)
 
 (provide 'dotemacs-lang)
 ;;; dotemacs-lang.el ends here
---
+++ modules/lang/dotemacs-lua.el
@@ -0,0 +1,30 @@
+;;; dotemacs-lua.el --- lua-mode. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(dotemacs-require-packages '(lua-mode company-lua))
+
+(defvar dotemacs-lua-mode-map (make-keymap)
+  "Keymap for Lua commands.")
+
+(use-package lua-mode
+  :defer t
+  :mode ("\\.lua\\'" . lua-mode)
+  :init
+  (add-to-list 'dotemacs-company-backend-alist '(lua-mode company-lua company-capf company-dabbrev-code company-yasnippet))
+  (add-to-list 'dotemacs-localleader-mode-map-alist '(lua-mode . dotemacs-lua-mode-map))
+  :config
+  (setq lua-indent-level 2)
+
+  (define-key dotemacs-lua-mode-map "b" 'lua-send-buffer)
+  (define-key dotemacs-lua-mode-map "l" 'lua-send-current-line)
+  (define-key dotemacs-lua-mode-map "f" 'lua-send-defun)
+  (define-key dotemacs-lua-mode-map "p" 'lua-send-proc)
+  (define-key dotemacs-lua-mode-map "r" 'lua-send-region)
+  (define-key dotemacs-lua-mode-map "z" 'lua-show-process-buffer))
+
+(use-package company-lua
+  :defer t)
+
+(provide 'dotemacs-lua)
+;;; dotemacs-lua.el ends here

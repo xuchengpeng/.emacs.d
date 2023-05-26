a151faae913f002f5b7428f6870031855b040732
xuchengpeng <330476629@qq.com>
2023-05-26 14:15:19 +0800

    fix(themes): load tokyonight styles with command

--- modules/dotemacs-themes.el
+++ modules/dotemacs-themes.el
@@ -3,12 +3,25 @@
 ;;; Code:
 
 ;;;###autoload
-(defun dotemacs-load-theme ()
-  "Load theme after init."
+(defun dotemacs-init-theme ()
+  "Init theme."
   (load "tokyonight-theme" 'nomessage t)
   (load-theme 'tokyonight t))
 
-(add-hook 'after-init-hook #'dotemacs-load-theme)
+(add-hook 'after-init-hook #'dotemacs-init-theme)
+
+(defun dotemacs-load-theme ()
+  "Load theme."
+  (interactive)
+  (let ((choice (completing-read
+                  "Select theme: "
+                  '("tokyonight-storm" "tokyonight-night" "tokyonight-moon" "tokyonight-day")))
+        (tokyonight-prefix "tokyonight-"))
+    (cond ((string-prefix-p tokyonight-prefix choice)
+           (setq tokyonight-theme-style (intern (substring choice (length tokyonight-prefix))))
+           (load-theme 'tokyonight t))
+          (t
+           (load-theme (intern choice) t)))))
 
 (provide 'dotemacs-themes)
 ;;; dotemacs-themes.el ends here

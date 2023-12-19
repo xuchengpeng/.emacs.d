3ad7419e49276b270bc65390e306463740ec17fa
xuchengpeng <330476629@qq.com>
2023-12-19 15:10:06 +0800

    fix(git): remove magit

--- modules/dotemacs-git.el
+++ modules/dotemacs-git.el
@@ -2,21 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(dotemacs-require-packages '(magit diff-hl))
-
-(use-package magit
-  :commands magit-file-delete
-  :init
-  (setq magit-auto-revert-mode nil)
-  (setq transient-levels-file  (concat dotemacs-cache-dir "transient/levels")
-        transient-values-file  (concat dotemacs-cache-dir "transient/values")
-        transient-history-file (concat dotemacs-cache-dir "transient/history")))
+(dotemacs-require-packages '(diff-hl))
 
 (use-package diff-hl
   :hook ((prog-mode after-save) . diff-hl-mode)
   :config
-  (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode))
 
 (provide 'dotemacs-git)
 ;;; dotemacs-git.el ends here

fdb8687942d1299245496825730b0c7aa4da3c66
xuchengpeng <330476629@qq.com>
2024-01-02 14:24:13 +0800

    fix(ox-publish): add site directory configuration

--- custom.example.el
+++ custom.example.el
@@ -8,21 +8,18 @@
 
 (setq dotemacs-tempel-path '("/path/to/templates"))
 
+(setq dotemacs-org-site-dir "d:/github/org-site/")
+
 (setq dotemacs-org-html-head
         "<link rel=\"shortcut icon\" href=\"images/rose-red.png\" type=\"image/x-icon\" />
          <link rel=\"stylesheet\" type=\"text/css\" href=\"/css/style.css\">"
       dotemacs-org-html-preamble-format
-        '(("en" "<header>
-                  <nav>
-                    <a href=\"/index.html\" class=\"current\">Home</a>
-                    <a href=\"/posts/index.html\">Posts</a>
-                    <a href=\"/about.html\">About</a>
-                  </nav>
-                  <h1>%t</h1>
-                </header>"))
+        '(("en" "<a href=\"/index.html\" style=\"text-decoration:underline\">Home</a>
+                 <a href=\"/posts/index.html\" style=\"text-decoration:underline\">Posts</a>
+                 <a href=\"/about.html\" style=\"text-decoration:underline\">About</a>"))
       dotemacs-org-html-postamble-format
-        '(("en" "<footer>
-                  <p>
-                    &copy xuchengpeng <a href=\"http://www.gnu.org/software/emacs/\">Emacs</a> 29.x  (<a href=\"https://orgmode.org\">Org</a> mode 9.x)
-                  </p>
-                </footer>")))
+        '(("en" "<div class=\"nav-btn\"><a href=\"/index.html\">Home</a></div>
+                 <div class=\"top-btn\"><a href=\"#top\">Top</a></div>
+                 <footer>
+                  <p>&copy xuchengpeng <a href=\"http://www.gnu.org/software/emacs/\">Emacs</a> 29.x  (<a href=\"https://orgmode.org\">Org</a> mode 9.x)</p>
+                 </footer>")))

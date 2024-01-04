d7acfd3eddb8000ee924f66c2a190a00b59ac036
xuchengpeng <330476629@qq.com>
2024-01-04 08:58:59 +0800

    fix(which-key): enable after init

--- modules/dotemacs-which-key.el
+++ modules/dotemacs-which-key.el
@@ -5,20 +5,9 @@
 (dotemacs-require-package 'which-key)
 
 (use-package which-key
-  :defer 1
-  :hook (pre-command . which-key-mode)
-  :init
-  (setq which-key-idle-delay 0.5
-        which-key-sort-order #'which-key-key-order-alpha
-        which-key-sort-uppercase-first nil
-        which-key-add-column-padding 1
-        which-key-max-display-columns nil
-        which-key-min-display-lines 6
-        which-key-side-window-slot -10)
+  :hook (after-init . which-key-mode)
   :config
-  (which-key-setup-side-window-bottom)
-  (add-hook 'which-key-init-buffer-hook (lambda () (setq line-spacing 3)))
-  (which-key-mode +1))
+  (setq which-key-add-column-padding 1))
 
 (provide 'dotemacs-which-key)
 ;;; dotemacs-which-key.el ends here

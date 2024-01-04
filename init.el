ca8408ee5a82a3a5372b68e3d51ceb0fa086ff32
xuchengpeng <330476629@qq.com>
2024-01-04 11:51:12 +0800

    fix(emacs): increase gc-cons-threshold

--- init.el
+++ init.el
@@ -1,17 +1,12 @@
 ;;; init.el --- dotemacs's configuration. -*- lexical-binding: t; -*-
-
 ;;; Commentary:
-;;
-;; Configuration entry.
-;;
-
 ;;; Code:
 
 (when (version< emacs-version "28.1")
   (error "Detected Emacs %s. Emacs version should be 28.1 or higher" emacs-version))
 
 (setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 16 1024 1024))))
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 20 1024 1024))))
 
 (defgroup dotemacs nil
   "Emacs framework."
@@ -25,11 +20,7 @@
 (defvar dotemacs-modules-dir (expand-file-name  "modules/" dotemacs-dir)
   "This directory houses all of the built-in dotemacs modules.")
 (defvar dotemacs-personal-dir (expand-file-name "personal/" dotemacs-dir)
-  "This directory is for your personal configuration.
-
-Users of Emacs dotemacs are encouraged to keep their personal configuration
-changes in this directory.  All Emacs Lisp files there are loaded automatically
-by dotemacs.")
+  "This directory is for your personal configuration.")
 (defvar dotemacs-personal-preload-dir (expand-file-name "preload/" dotemacs-personal-dir)
   "This directory is for your personal configuration loaded before dotemacs.")
 (defvar dotemacs-modules-file (expand-file-name "dotemacs-modules.el" dotemacs-personal-dir)
@@ -42,9 +33,6 @@ by dotemacs.")
 ;; add dotemacs's directories to Emacs's `load-path'
 (add-to-list 'load-path dotemacs-core-dir)
 (add-to-list 'load-path dotemacs-modules-dir)
-
-;; warn when opening files bigger than 100MB
-(setq large-file-warning-threshold 100000000)
 
 ;; config changes made through the customize UI will be stored here
 (setq custom-file (expand-file-name "custom.el" dotemacs-local-dir))

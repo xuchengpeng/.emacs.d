a26e2efed0cda20832b19b6349bfa2fda31dfbc5
xuchengpeng <330476629@qq.com>
2024-01-15 15:00:26 +0800

    fix(perf): save startup time

--- core/dotemacs-ui.el
+++ core/dotemacs-ui.el
@@ -44,11 +44,11 @@
     (setq use-short-answers t)
   (fset 'yes-or-no-p 'y-or-n-p))
 
-(setq-default major-mode 'text-mode)
+(setq-default major-mode 'fundamental-mode)
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message user-login-name
       inhibit-default-init t
-      initial-major-mode 'text-mode
+      initial-major-mode 'fundamental-mode
       initial-scratch-message nil)
 (advice-add #'display-startup-echo-area-message :override #'ignore)
 (advice-add #'display-startup-screen :override #'ignore)

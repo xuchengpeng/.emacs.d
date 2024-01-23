0882c18a5de71aff5f04bbaed8e69f179e5e2b1e
xuchengpeng <330476629@qq.com>
2024-01-23 18:41:03 +0800

    fix(markdown): preview with pandoc

--- modules/lang/dotemacs-markdown.el
+++ modules/lang/dotemacs-markdown.el
@@ -4,39 +4,9 @@
 
 (dotemacs-require-package 'markdown-mode)
 
-(defun dotemacs-markdown-compile-marked (beg end output-buffer)
-  "Compiles markdown with the marked program, if available.
-Returns its exit code."
-  (when (executable-find "marked")
-    (apply #'call-process-region
-           beg end "marked" nil output-buffer nil
-           (when (eq major-mode 'gfm-mode)
-             (list "--gfm" "--tables" "--breaks")))))
-
-(defun dotemacs-markdown-compile-pandoc (beg end output-buffer)
-  "Compiles markdown with the pandoc program, if available.
-Returns its exit code."
-  (when (executable-find "pandoc")
-    (call-process-region beg end "pandoc" nil output-buffer nil
-                         "-f" "markdown"
-                         "-t" "html"
-                         "--mathjax"
-                         "--highlight-style=pygments")))
-
-(defvar dotemacs-markdown-compile-functions
-  '(dotemacs-markdown-compile-marked
-    dotemacs-markdown-compile-pandoc)
-  "A list of markdown compile commands.")
-
-(defun dotemacs-markdown-compile (beg end output-buffer)
-  "Compile markdown into html."
-  (or (run-hook-with-args-until-success 'dotemacs-markdown-compile-functions
-                                        beg end output-buffer)
-      (user-error "No markdown program could be found. Install marked, pandoc, markdown or multimarkdown.")))
-
 (defun dotemacs-markdown-toc ()
   "Extract headings from the current Markdown buffer.
-   The generated and indented TOC will be inserted at point."
+The generated and indented TOC will be inserted at point."
   (interactive)
   (let (toc-list markdown-toc)
     (save-excursion
@@ -63,7 +33,26 @@ Returns its exit code."
   :init
   (add-to-list 'dotemacs-major-mode-map-alist '(markdown-mode . markdown-mode-command-map))
   :config
-  (setq markdown-command #'dotemacs-markdown-compile)
+  (setq markdown-enable-math t
+        markdown-enable-wiki-links t
+        markdown-italic-underscore t
+        markdown-asymmetric-header t
+        markdown-make-gfm-checkboxes-buttons t
+        markdown-gfm-uppercase-checkbox t
+        markdown-fontify-whole-heading-line t
+        markdown-content-type "application/xhtml+xml"
+        markdown-css-paths
+        '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
+          "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
+        markdown-xhtml-header-content
+        (concat "<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"
+                "<style> body { box-sizing: border-box; max-width: 740px; width: 100%; margin: 40px auto; padding: 0 10px; } </style>"
+                "<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>"
+                "<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css'>"
+                "<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>"
+                "<script>document.addEventListener('DOMContentLoaded', () => { document.body.classList.add('markdown-body'); document.querySelectorAll('pre[lang] > code').forEach((code) => { code.classList.add(code.parentElement.lang); }); document.querySelectorAll('pre > code').forEach((code) => { hljs.highlightBlock(code); }); });</script>"))
+  (when (executable-find "pandoc")
+    (setq markdown-command '("pandoc" "--from=markdown" "--to=html5" "--mathjax" "--highlight-style=pygments")))
   (define-key markdown-mode-command-map "i" markdown-mode-style-map))
 
 (provide 'dotemacs-markdown)

c139c44b99ea2d70ed3da33e903fb675f442f416
xuchengpeng <330476629@qq.com>
2024-03-21 17:20:30 +0800

    fix(vterm): show default directory in modeline

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -12,7 +12,7 @@
      'vterm-mode-hook
      (lambda ()
        (setq-local dotemacs-modeline-left '(dotemacs-modeline--window-number
-                                            dotemacs-modeline--buffer-info)
+                                            dotemacs-modeline--buffer-default-directory)
                    dotemacs-modeline-right '(dotemacs-modeline--major-mode))))))
 
 (defun dotemacs-shell ()

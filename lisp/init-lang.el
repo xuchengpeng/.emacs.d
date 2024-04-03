63c7f7e7c4254372fd39b7732ea46c10dd290751
xuchengpeng <330476629@qq.com>
2024-04-03 16:54:27 +0800

    fix(indent): C/C++ mode indent with 4

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -4,6 +4,13 @@
 
 (push (expand-file-name "lisp/lang" dotemacs-dir) load-path)
 
+(with-eval-after-load 'cc-mode
+  (setq-default c-basic-offset 4))
+
+(when (dotemacs-treesit-available-p)
+  (with-eval-after-load 'c-ts-mode
+    (setq c-ts-mode-indent-offset 4)))
+
 (require 'init-markdown)
 (require 'init-lua)
 

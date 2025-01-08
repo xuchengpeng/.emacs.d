e5448aa67afc7aa747ad9c3ee9bf66237fa0f787
xuchengpeng <330476629@qq.com>
2025-01-08 17:15:46 +0800

    Revert "fix(org): remove reference override"
    This reverts commit cf275e092be40f2a0d9db5161e2ade7778240f06.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -98,6 +98,55 @@
         org-html-head-include-scripts nil
         org-html-validation-link nil)
 
+  (defcustom +org-html-stable-ids t
+    "Non-nil means to use stable IDs in the exported document."
+    :type 'boolean)
+
+  (defun +org-html-stable-ids-extract-id (datum)
+    "Extract a reference from a DATUM.
+
+Return DATUM's `:CUSTOM_ID` if set, or generate a reference from its
+`:raw-value` property.  If the DATUM does not have either, return
+nil."
+    (or
+     (org-element-property :CUSTOM_ID datum)
+     (let ((value (org-element-property :raw-value datum)))
+       (when value
+         (+org-html-stable-ids-to-kebab-case value)))))
+
+  (defun +org-html-stable-ids-to-kebab-case (string)
+    "Convert STRING to kebab-case."
+    (string-trim
+     (replace-regexp-in-string
+      "[^a-z0-9]+" "-"
+      (downcase string))
+     "-" "-"))
+
+  (defun +org-export-stable-ids-get-reference (datum info)
+    "Return a reference for DATUM with INFO.
+
+    Raise an error if the ID was used in the document before."
+    (let ((cache (plist-get info :internal-references))
+	      (id (+org-html-stable-ids-extract-id datum)))
+	  (or (car (rassq datum cache))
+	      (if (assoc id cache)
+		      (user-error "Duplicate ID: %s" id)
+	        (when id
+		      (push (cons id datum) cache)
+		      (plist-put info :internal-references cache)
+		      id)))))
+
+  (defun +org-html-stable-ids-reference (datum info &optional named-only)
+    "Call `org-export-get-reference` to get a reference for DATUM with INFO.
+
+If `NAMED-ONLY` is non-nil, return nil."
+    (unless named-only
+      (org-export-get-reference datum info)))
+
+  (when +org-html-stable-ids
+    (advice-add #'org-export-get-reference :override #'+org-export-stable-ids-get-reference)
+    (advice-add #'org-html--reference :override #'+org-html-stable-ids-reference))
+
   (defun +org-publish-sitemap (title list)
     (concat "#+TITLE: " title "\n"
             "#+OPTIONS: toc:nil\n\n"

3b2bcdf59f49af70e431ca438012312e4e641989
xuchengpeng <chuckxcp@qq.com>
2025-04-15 15:42:53 +0800

    fix(dabbrev): ignore authinfo-mode

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -131,6 +131,7 @@
   (keymap-global-set "C-M-/" 'dabbrev-expand)
   :config
   (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  (add-to-list 'dabbrev-ignored-buffer-modes 'authinfo-mode)
   (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
   (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
   (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))

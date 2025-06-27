388c1e2929b5c129f7cb7ac620c87245484b122f
xuchengpeng <chuckxcp@qq.com>
2025-06-27 08:39:19 +0800

    fix(echo-bar): add project name instead of tar bar name

--- lisp/init-echo-bar.el
+++ lisp/init-echo-bar.el
@@ -11,7 +11,7 @@
   :group 'applications)
 
 (defcustom echo-bar-modules
-  '(echo-bar--tab-bar-name
+  '(echo-bar--project
     echo-bar--eglot
     echo-bar--selection-info
     echo-bar--word-count
@@ -103,12 +103,11 @@ If nil, don't update the echo bar automatically."
 (defvar text-scale-mode-lighter)
 
 (declare-function mc/num-cursors "ext:multiple-cursors-core")
-(declare-function tab-bar--current-tab "tab-bar")
 
-(defun echo-bar--tab-bar-name ()
-  "Tab bar name."
-  (when (fboundp 'tab-bar-mode)
-    (propertize (alist-get 'name (tab-bar--current-tab)) 'face 'echo-bar-blue-face)))
+(defun echo-bar--project ()
+  "Project name."
+  (when-let* ((name (project-name (project-current))))
+    (propertize name 'face 'echo-bar-blue-face)))
 
 (defun echo-bar--text-scale ()
   "Text-Scale info."

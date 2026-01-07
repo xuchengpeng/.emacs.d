2c8e3f5aca7a2cb5f46393b9a3957a7065c93d07
xuchengpeng <chuckxcp@qq.com>
2026-01-07 11:22:53 +0800

    fix(markdown): render math with katex

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -24,7 +24,16 @@
         markdown-xhtml-header-content
         (concat "<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"
                 "<style> *, ::after, ::before, ::backdrop, ::file-selector-button { box-sizing: border-box; margin: 0; padding: 0; border: 0 solid; } body { max-width: 72rem; width: 100%; margin: 0 auto; padding: 1em; } </style>"
-                "<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js'></script>"
+                "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/katex@0.16.27/dist/katex.min.css\" integrity=\"sha384-Pu5+C18nP5dwykLJOhd2U4Xen7rjScHN/qusop27hdd2drI+lL5KvX7YntvT8yew\" crossorigin=\"anonymous\">"
+                "<script type=\"module\">
+  import renderMathInElement from \"https://cdn.jsdelivr.net/npm/katex@0.16.27/dist/contrib/auto-render.mjs\";
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: '$$', right: '$$', display: true},
+      {left: '$', right: '$', display: false}
+    ],
+  });
+</script>"
                 "<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>"
                 "<script>hljs.highlightAll();</script>")
         markdown-command "marked"))

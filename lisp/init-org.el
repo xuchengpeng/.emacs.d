e440641200c41498d9a0fb87bcce122c86a750a1
xuchengpeng <chuckxcp@qq.com>
2026-01-14 11:25:45 +0800

    fix(org): add custom agenda view

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -18,8 +18,31 @@
         org-agenda-window-setup 'current-window
         org-persist-directory (expand-file-name "org-persist" dotemacs-cache-dir)
         org-startup-indented t
+        org-log-into-drawer t
         org-tags-column 0
-        org-agenda-tags-column 0)
+        org-agenda-tags-column 0
+        org-agenda-custom-commands
+        '(("d" "Daily Agenda"
+           ((tags-todo "+PRIORITY=\"A\""
+                       ((org-agenda-overriding-header "Important tasks")))
+            (agenda "" ((org-agenda-overriding-header "Today")
+                        (org-agenda-span 1)
+                        (org-deadline-warning-days 0)
+                        (org-scheduled-past-days 0)))
+            (agenda "" ((org-agenda-overriding-header "Next 3 days")
+                        (org-agenda-start-on-weekday nil)
+                        (org-agenda-start-day "+1d")
+                        (org-agenda-span 3)
+                        (org-deadline-warning-days 0)
+                        (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))))
+            (agenda "" ((org-agenda-overriding-header "Upcoming 14 days")
+                        (org-agenda-time-grid nil)
+                        (org-agenda-start-on-weekday nil)
+                        (org-agenda-start-day "+4d")
+                        (org-agenda-span 14)
+                        (org-agenda-show-all-dates nil)
+                        (org-deadline-warning-days 0)
+                        (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))))))))
 
   (add-hook
    'org-agenda-mode-hook
